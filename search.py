197b1751c738989e726743b9036bf8f2977629fb
MarshallKnisal <Mskgmf@umsystem.edu>
2026-03-09 18:05:20 -0500

    Implemented a depth first search algorithm using the LIFO/stack data structure. the function adds each neighboring cell to the stack, then pops the last one added. If there is no traversable path at that depth, the stack continues to pop until a new path is found, resembling the backtracking factor of DFS.

--- search.py
+++ search.py
@@ -26,4 +26,27 @@ def bfs(grid, start, goal):
 
     return parent
 
-#def dsf(grid, start, goal)
+
+def dsf(grid, start, goal):
+    if not grid.in_bounds(start) or not grid.is_traversable(start):
+        raise ValueError("Start Position is invalid or not traversable")
+    if not grid.in_bounds(goal) or not grid.is_traversable(goal):
+        raise ValueError("Goal Position is invalid or not traversable")
+
+    parent = {start: None}
+    visited = {start}
+    stack = [start]
+
+    while stack:
+        current = stack.pop()
+
+        if current == goal:
+            return parent
+
+        for neighbor in grid.get_neighbors(current):
+            if neighbor not in visited:
+                visited.add(neighbor)
+                parent[neighbor] = current
+                stack.append(neighbor)
+
+    return parent
